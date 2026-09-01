0b99b27a5fa0c76fe94f4904b7ad70f80c7f5789
Lisbeth Yang <lisbeth_xinyi@hotmail.com>
2026-09-01 13:53:31 +0200

    chore: added entity and enum classes

--- src/main/java/com/codecool/paws_and_requests/model/Post.java
+++ src/main/java/com/codecool/paws_and_requests/model/Post.java
@@ -1,7 +1,31 @@
 package com.codecool.paws_and_requests.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/com/codecool/paws_and_requests/model/User.java
+++ src/main/java/com/codecool/paws_and_requests/model/User.java
@@ -1,9 +1,40 @@
 package com.codecool.paws_and_requests.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
+
+    private String profilePicture;
+    private String organizationNumber;
 }
