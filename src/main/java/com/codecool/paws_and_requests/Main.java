68e9c839beda1f89e8a47a7d2eb0b8f11fcf7bc2
Lisbeth Yang <lisbeth_xinyi@hotmail.com>
2026-09-01 13:04:28 +0200

    chore: basic setup of empty models and repositories

--- src/main/java/com/codecool/paws_and_requests/Main.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.paws_and_requests;
-
-public class Main {
-    public static void main(final String[] args) {
-        int X = 0;
-        System.out.println("Hello World!");
-    }
-}
