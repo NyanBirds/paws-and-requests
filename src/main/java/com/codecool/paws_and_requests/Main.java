1b85c69fed429f7364750e2caa5082be1e9565c0
Sara <paulinasara@live.no>
2026-09-01 13:06:54 +0200

    chore: remove error caused by checkstyle in the main.java

--- src/main/java/com/codecool/paws_and_requests/Main.java
+++ src/main/java/com/codecool/paws_and_requests/Main.java
@@ -2,7 +2,6 @@ package com.codecool.paws_and_requests;
 
 public class Main {
     public static void main(final String[] args) {
-        int X = 0;
         System.out.println("Hello World!");
     }
 }
