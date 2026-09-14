9aa0a0d7b83b34eb6581e741198a3d56d3ed82d4
Lisbeth Yang <lisbeth_xinyi@hotmail.com>
2026-09-14 12:38:16 +0200

    feat: shelters can add a new animal

--- src/main/java/com/codecool/pawsandrequests/mapper/AnimalMapper.java
+++ src/main/java/com/codecool/pawsandrequests/mapper/AnimalMapper.java
@@ -1,4 +1,16 @@
 package com.codecool.pawsandrequests.mapper;
 
-public class AnimalMapper {
+import com.codecool.pawsandrequests.dto.AnimalRequest;
+import com.codecool.pawsandrequests.dto.AnimalResponse;
+import com.codecool.pawsandrequests.model.Animal;
+import com.codecool.pawsandrequests.model.Shelter;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AnimalMapper {
+    AnimalResponse toAnimalResponse(Animal animal);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "shelter", source = "shelter")
+    Animal toAnimal(AnimalRequest animalRequest, Shelter shelter);
 }
--- src/main/java/com/codecool/pawsandrequests/service/AnimalService.java
+++ src/main/java/com/codecool/pawsandrequests/service/AnimalService.java
@@ -1,6 +1,10 @@
 package com.codecool.pawsandrequests.service;
 
+import com.codecool.pawsandrequests.dto.AnimalRequest;
 import com.codecool.pawsandrequests.dto.AnimalResponse;
+import com.codecool.pawsandrequests.mapper.AnimalMapper;
+import com.codecool.pawsandrequests.model.Animal;
+import com.codecool.pawsandrequests.model.Shelter;
 import com.codecool.pawsandrequests.repository.AnimalRepository;
 import com.codecool.pawsandrequests.repository.ShelterRepository;
 import jakarta.transaction.Transactional;
@@ -14,18 +18,19 @@ public class AnimalService {
 
     private final AnimalRepository animalRepository;
     private final ShelterRepository shelterRepository;
+    private final AnimalMapper animalMapper;
 
     public AnimalService(
             final AnimalRepository animalRepo,
-            final ShelterRepository shelterRepo
+            final ShelterRepository shelterRepo,
+            final AnimalMapper mapper
     ) {
         this.animalRepository = animalRepo;
         this.shelterRepository = shelterRepo;
+        this.animalMapper = mapper;
     }
 
     public final List<AnimalResponse> getMyAnimals(final String orgNr) {
-
-
         return animalRepository.findAll().stream()
                 .filter(animal -> animal.getShelter()
                         .equals(shelterRepository.findByOrgNr(orgNr).get())
@@ -37,5 +42,14 @@ public class AnimalService {
                         animal.getSpecies()
                 ))
                 .toList();
+    }
+
+    public final void addAnimal(
+            final String orgNr,
+            final AnimalRequest request
+    ) {
+        Shelter shelter = shelterRepository.findByOrgNr(orgNr).get();
+        Animal animal = animalMapper.toAnimal(request, shelter);
+        animalRepository.save(animal);
     }
 }
