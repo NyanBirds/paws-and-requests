f56c9c25975fd6dd85de32a23b60e218bb3ca747
Lisbeth Yang <lisbeth_xinyi@hotmail.com>
2026-09-16 12:20:28 +0200

    feat: add postmapper and filtering options for posts

--- src/main/java/com/codecool/pawsandrequests/dto/PostSummaryResponse.java
+++ src/main/java/com/codecool/pawsandrequests/dto/PostSummaryResponse.java
@@ -3,7 +3,10 @@ package com.codecool.pawsandrequests.dto;
 import com.codecool.pawsandrequests.model.Gender;
 import com.codecool.pawsandrequests.model.Species;
 
+import java.util.UUID;
+
 public record PostSummaryResponse(
+        UUID id,
         String title,
         int age,
         Gender gender,
