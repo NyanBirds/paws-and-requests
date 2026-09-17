489d07bdb386e6a18275bf50aba9edce89c79101
Vegard <vegard.borvik@no.experis.com>
2026-09-17 15:31:30 +0200

    feat: shelter posts displayd on shelter page

--- frontend/src/pages/ShelterPage.jsx
+++ frontend/src/pages/ShelterPage.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import { api } from "../api/client";
 import { useParams } from "react-router";
+import PostDetails from "../components/PostDetails";
 
 export default function ShelterPage() {
     const { orgNr } = useParams()
     const [shelter, setShelter] = useState()
+    const [posts, setPosts] = useState([])
     const [error, setError] = useState()
 
     useEffect(() => {
         api.get(`/shelters/${orgNr}`)
             .then(res => setShelter(res), 
                   err => setError(err))
+        api.get(`/posts/shelter/${orgNr}`)
+            .then(res => setPosts(res))
+            .then(console.log(posts))
     }, [])
 
     return <>
@@ -19,9 +24,12 @@ export default function ShelterPage() {
     ) : (
         <div>
             <h1>{shelter?.shelterName}</h1>
-            <p>{shelter?.description}</p>
-            <p>{shelter?.address}</p>
-            
+            <p>About us: {shelter?.description}</p>
+            <p>Address: {shelter?.address}</p>
+            <section>
+                <h2>Posts</h2>
+                {posts.map(post => <PostDetails key={post.id} {...post}/>)}
+            </section>
         </div>
     )}
     </>
