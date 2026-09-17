ad941dd03840f36e3dbe2d2df7f9486946fede1a
Sara <paulinasara@live.no>
2026-09-17 13:55:20 +0200

    feat: endpoint for create a post

--- src/main/java/com/codecool/pawsandrequests/controller/PostController.java
+++ src/main/java/com/codecool/pawsandrequests/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.codecool.pawsandrequests.controller;
 
+import com.codecool.pawsandrequests.dto.PostRequest;
 import com.codecool.pawsandrequests.dto.PostResponse;
 import com.codecool.pawsandrequests.dto.PostSummaryResponse;
+import com.codecool.pawsandrequests.model.CustomUserDetails;
 import com.codecool.pawsandrequests.model.Gender;
 import com.codecool.pawsandrequests.model.Species;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +13,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +43,20 @@ public final class PostController {
     @GetMapping("/{postId}")
     public PostResponse getOnePost(@PathVariable final UUID postId) {
         return service.getOnePost(postId);
+    }
+
+    @PostMapping()
+    public ResponseEntity<PostResponse> createPost(
+            @AuthenticationPrincipal final CustomUserDetails customUserDetails,
+            @RequestBody final PostRequest postRequest) {
+        PostResponse response = service.createPost(postRequest.title(),
+                postRequest.description(),
+                postRequest.url(),
+                customUserDetails.getOrgNr(),
+                postRequest.animalId(),
+                customUserDetails.getUsername()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{postId}")
