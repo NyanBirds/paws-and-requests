e4b5ef6c61af2ca520dcc47caacf62fe246550d5
Sara <paulinasara@live.no>
2026-09-23 12:23:01 +0200

    feat: frontend using blobs is created :)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import RegisterPage from "./pages/RegisterPage.jsx";
 import AnimalProfilePage from './pages/AnimalProfilePage.jsx';
 import PostsPage from "./pages/PostsPage.jsx";
 import {AdoptionPage} from "./pages/AdoptionPage.jsx";
+import CreatePost from './pages/CreatePost.jsx';
 
 const router = createBrowserRouter([
     {
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
             { path: 'login', Component: LoginPage },
             { path: 'posts', Component: PostsPage },
             { path: 'posts/:postId', Component: AnimalProfilePage },
-            { path: 'posts/:postId/adoption', Component: AdoptionPage }
+            { path: 'posts/:postId/adoption', Component: AdoptionPage },
+            { path: 'post/new', Component: CreatePost }
         ]
     }
 ]);
