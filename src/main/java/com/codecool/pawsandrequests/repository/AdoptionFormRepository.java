31cb7928c14a48c630a8f86f042f6f19e67e2d69
Vegard <vegard.borvik@no.experis.com>
2026-09-25 13:04:44 +0200

    feat: add /adoptionforms endpoint

--- src/main/java/com/codecool/pawsandrequests/repository/AdoptionFormRepository.java
+++ src/main/java/com/codecool/pawsandrequests/repository/AdoptionFormRepository.java
@@ -1,12 +1,15 @@
 package com.codecool.pawsandrequests.repository;
 
 import com.codecool.pawsandrequests.model.AdoptionForm;
+import com.codecool.pawsandrequests.model.Shelter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AdoptionFormRepository extends
         JpaRepository<AdoptionForm, UUID> {
+    List<AdoptionForm> findByPostUserShelterOrgNr(String userShelterOrgNr);
 }
